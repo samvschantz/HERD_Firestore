a6e513d587cdbc5ba80924d5766da0bc26e0e018
Samuel Schantz <Samuel Schantz@tbadigital.local>
2019-06-13 14:32:20 -0700

    Added sign in form functions very similarly to signup

--- herd_firestore/src/components/SignIn/index.js
+++ herd_firestore/src/components/SignIn/index.js
@@ -1,12 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
-function SignIn(props) {
+import { SignUpLink } from '../SignUp';
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+
+const SignInPage = () => (
+    <div>
+        <h1>SignIn</h1>
+        <SignInForm />
+        <SignUpLink />
+    </div>
+);
+
+function SignInFormBase(props) {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+
+    const isEmptyPwd = password === '';
+
+    const isEmptyEmail = email === '';
+
+    const isInvalid = isEmptyEmail || isEmptyPwd;
+
+    const onBlur = (e) => {
+        switch(e.target.name){
+            case 'username':
+                isEmptyPwd ? setError({message: 'Password required'}):setError(null);
+                break;
+            default:
+                isEmptyEmail ? setError({message: 'Email required'}):setError(null);
+                break;
+        }
+    }
+    
+    const onSubmit = (e) => {
+        props.withFirebase
+            .doSignInWithEmailAndPassword(email, password)
+            .then(() => {
+                props.history.push(ROUTES.HOME);
+            })
+            .catch(error => {
+                setError(error);
+            })
+        e.preventDefault();
+    }
+    
+    const onChange = (e) => {
+        switch(e.target.name){
+            case 'email':
+                setEmail(e.target.value);
+                break;
+            case 'password':
+                setPassword(e.target.value);
+                break;
+            default:
+                setError(e.target.value);
+                break;
+        }
+    }
 
     return (
-        <div>
-	         <h1>SignIn</h1>
-        </div>
+        <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          value={email}
+          onChange={(e) => onChange(e)}
+          onBlur={(e) => onBlur(e)}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          name="password"
+          value={password}
+          onChange={(e) => onChange(e)}
+          onBlur={(e) => onBlur(e)}
+          type="password"
+          placeholder="Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Sign In
+        </button>
+
+        {error && <p>{error.message}</p>}
+      </form>
     )
 }
 
-export default SignIn;
+const SignInForm = compose(
+    withRouter,
+    withFirebase,
+  )(SignInFormBase);
+  
+  export default SignInPage;
+  
+  export { SignInForm };
